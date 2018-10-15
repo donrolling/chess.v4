bf2b81e673ae0c816b19ab39efbc4f030a36452a
Don Rolling <donrolling@hotmail.com>
2018-10-14 23:04:54 -0500

    add MoveInfo class

---
+++ engine/model/MoveInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace chess.v4.engine.model {
+
+	public class MoveInfo {
+		public bool HasThreefoldRepition { get; set; }
+		public bool IsBlackCheck { get; set; }
+		public bool IsCastle { get; set; }
+		public bool IsCheck { get { return IsWhiteCheck || IsBlackCheck; } }
+		public bool IsCheckmate { get; set; }
+		public bool IsEnPassant { get; set; }
+		public bool IsPawnPromotion { get; set; }
+		public bool IsWhiteCheck { get; set; }
+		public bool PutsOwnKingInCheck { get; set; }
+	}
+}
