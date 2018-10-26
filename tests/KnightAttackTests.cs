dcf2ae88c93f20993a56038115deb4c252a32459
Don Rolling <donrolling@hotmail.com>
2018-10-25 22:41:04 -0500

    Fixed WhiteKnightAttackStartingPositionFromB4 Refactored the class as well. Better reporting on issues with failed tests now.

--- tests/KnightAttackTests.cs
+++ tests/KnightAttackTests.cs
@@ -5,6 +5,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using tests.setup;
 using tests.utility;
+using System.Collections.Generic;
+using chess.v4.engine.enumeration;
 
 namespace tests {
 
@@ -26,36 +28,30 @@ namespace tests {
 		public void WhiteKnightAttackStartingPositionFromD5() {
 			var fen = "rnbqkbnr/pppppppp/8/3N4/8/8/PPPPPPPP/R1BQKBNR b KQkq - 0 1";
 			var gameState = TestUtility.GetGameState(this.GameStateService, fen);
-			var knightAttacks = gameState.Attacks.Where(a => a.AttackerSquare.Name == "d5").ToList();
-			var allSquareIndexs = new int[] { 18, 20, 25, 29, 41, 45, 50, 52 };
-			foreach (var x in allSquareIndexs) {
-				Assert.IsNotNull(knightAttacks.GetSquare(x), $"Knight should be able to attack square: { x }");
-			}
-			Assert.IsTrue(knightAttacks.Count() == 8);
+			var attacks = gameState.Attacks.Where(a => a.AttackerSquare.Name == "d5").ToList();
+			var squares = new List<int> { 18, 20, 25, 29, 41, 45, 50, 52 };
+			TestUtility.ListContainsSquares(attacks, squares, PieceType.Knight);
+			Assert.IsTrue(attacks.Count() == 8, "Wrong number of attacks.");
 		}
 
 		[TestMethod]
 		public void WhiteKnightAttackStartingPositionFromB1() {
 			var fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 			var gameState = TestUtility.GetGameState(this.GameStateService, fen);
-			var knightAttacks = gameState.Attacks.Where(a => a.AttackerSquare.Name == "b1").ToList();
-			var allSquareIndexes = new int[] { 18, 16 };
-			foreach (var x in allSquareIndexes) {
-				Assert.IsNotNull(knightAttacks.GetSquare(x), $"Knight should be able to attack square: { x }");
-			}
-			Assert.IsTrue(knightAttacks.Count() == 2);
+			var attacks = gameState.Attacks.Where(a => a.AttackerSquare.Name == "b1").ToList();
+			var squares = new List<int> { 18, 16 };
+			TestUtility.ListContainsSquares(attacks, squares, PieceType.Knight);
+			Assert.IsTrue(attacks.Count() == 2, "Wrong number of attacks.");
 		}
 
 		[TestMethod]
 		public void WhiteKnightAttackStartingPositionFromB4() {
-			var fen = "rnbqkbnr/pppppppp/8/3N4/8/8/PPPPPPPP/R1BQKBNR b KQkq - 0 1";
+			var fen = "rnbqkbnr/pppppppp/8/8/1N6/8/PPPPPPPP/R1BQKBNR b KQkq - 0 1";
 			var gameState = TestUtility.GetGameState(this.GameStateService, fen);
-			var knightAttacks = gameState.Attacks.Where(a => a.AttackerSquare.Name == "b4").ToList();
-			var allSquareIndexs = new int[] { 19, 35, 40, 42 };
-			foreach (var x in allSquareIndexs) {
-				Assert.IsNotNull(knightAttacks.GetSquare(x), $"Knight should be able to attack square: { x }");
-			}
-			Assert.IsTrue(knightAttacks.Count() == 4);
+			var attacks = gameState.Attacks.Where(a => a.AttackerSquare.Name == "b4").ToList();
+			var squares = new List<int> { 19, 35, 40, 42 };
+			TestUtility.ListContainsSquares(attacks, squares, PieceType.Knight);
+			Assert.IsTrue(attacks.Count() == 4, "Wrong number of attacks.");
 		}
 	}
 }
