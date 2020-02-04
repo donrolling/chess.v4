0d7eff7694e77f7a65baf95d6d23734a7aa3fc9d
Don <donrolling@gmail.com>
2020-02-04 00:16:58 -0600

    step one, create the site

---
+++ Website/Typescript/Index.ts
@@ -0,0 +1,10 @@
+﻿class Index {
+    private readonly FENInputId: string = "#FEN";
+    private FENInput: string;
+
+    constructor() {
+        this.FENInput = (document.querySelector(this.FENInputId) as HTMLInputElement).value;
+        alert(this.FENInput);
+    }
+
+}
---
+++ Website/wwwroot/ts/Index.js
@@ -0,0 +1,9 @@
+var Index = /** @class */ (function () {
+    function Index() {
+        this.FENInputId = "#FEN";
+        this.FENInput = document.querySelector(this.FENInputId).value;
+        alert(this.FENInput);
+    }
+    return Index;
+}());
+//# sourceMappingURL=Index.js.map
