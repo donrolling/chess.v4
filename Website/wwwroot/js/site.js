bcfb3973e77da81b3f29362233b6051521561635
Don <donrolling@gmail.com>
2020-02-09 23:20:51 -0600

    Got rid of typescript because chessboardjs doesn't play nice with it.

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿var index = new Index();
+﻿$(document).ready(function () {
+    setup();
+});
+
+var setup = function () {
+    getFenAndUpdate();;
+    $('#fenSubmit').click(function () {
+        getFenAndUpdate();
+    });
+};
+
+var getFenAndUpdate = function () {
+    fen = $('.fen').val();
+    updateFen(fen);
+}
+
+var updateFen = function (fen) {
+    board = Chessboard('chessBoard', fen);
+};
+
--- Website/wwwroot/ts/Index.js
+++
@@ -1,9 +0,0 @@
-var Index = /** @class */ (function () {
-    function Index() {
-        this.FENInputId = "#FEN";
-        this.FENInput = document.querySelector(this.FENInputId).value;
-        alert(this.FENInput);
-    }
-    return Index;
-}());
-//# sourceMappingURL=Index.js.map
