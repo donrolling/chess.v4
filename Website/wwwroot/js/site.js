99f30199b85e4179a01ce28b23366bc179ab3bd9
Don <donrolling@gmail.com>
2020-02-10 23:09:07 -0600

    got the piece moves to display on click!

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -1,4 +1,6 @@
 ﻿var board;
+var gamestate;
+var attacks;
 
 $(document).ready(function () {
     setup();
@@ -29,9 +31,53 @@ var updateFen = function (fen) {
         contentType: 'application/json',
         dataType: 'json'
 
-    }).done(function (gamestate) {
-        console.log(gamestate);
+    }).done(function (gamestateResult) {
+        gamestate = gamestateResult.result;
+        attacks = gamestate.attacks;
         board = Chessboard('chessBoard', fen);
+        $('.square-55d63').click(function (e) {
+            handleSquareClick(e);
+        });
     });
+};
+
+var getCurrentSquare = function (e) {
+    var obj = $(e.target);
+    var piece = obj.data('piece');
+    if (piece) {
+        obj = $(e.target).parent();
+    }
+    return obj.data('square');
+};
+
+var getSquareAttacks = function(square) {
+    var squareAttacks = [];
+    for (var i = 0; i < attacks.length; i++) {
+        var attack = attacks[i];
+        if (attack.attackingSquare.name === square) {
+            squareAttacks.push(attack);
+        }
+    }
+    return squareAttacks;
+};
+
+var highlightSquares = function (attacks) {
+    for (var i = 0; i < attacks.length; i++) {
+        var attack = attacks[i];
+        var squareClass = attack.isProtecting ? 'protecting' : 'attacking';
+        $('.square-' + attack.name).addClass(squareClass);
+    }
+};
+
+var removeOldClasses = function () {
+    $('.attacking').removeClass('attacking');
+    $('.protecting').removeClass('protecting');
+};
+
+var handleSquareClick = function (e) {
+    removeOldClasses();
+    var currentSquare = getCurrentSquare(e);
+    var squareAttacks = getSquareAttacks(currentSquare);
+    highlightSquares(squareAttacks);
 };
 
