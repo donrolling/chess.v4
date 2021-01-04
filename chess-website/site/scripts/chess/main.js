60c4b31694e7f095dd95d506ffe3013052341c31
Don <donrolling@gmail.com>
2021-01-04 00:22:51 -0600

    cleanup

--- chess-website/site/scripts/chess/main.js
+++ chess-website/site/scripts/chess/main.js
@@ -1,6 +1,5 @@
 define(function (require) {
     const chessModule = require('./chess');
-    //require('requirejs-text/text');
     var json = require('text!../../appsettings.json');
     if (json) {
         new chessModule.chess(JSON.parse(json));
