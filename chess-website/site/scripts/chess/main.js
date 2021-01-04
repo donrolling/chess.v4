5d6ddd1d96afa4c243a278a8b66cf49a8e4e1d16
Don <donrolling@gmail.com>
2021-01-04 00:22:31 -0600

    got stuff working locally...or at least we're to the CORS issues

--- chess-website/site/scripts/chess/main.js
+++ chess-website/site/scripts/chess/main.js
@@ -2,8 +2,7 @@ define(function (require) {
     const chessModule = require('./chess');
     //require('requirejs-text/text');
     var json = require('text!../../appsettings.json');
-    console.log(json);
-    // if (json) {
-    //     new chessModule.chess(JSON.parse(config));
-    // }
+    if (json) {
+        new chessModule.chess(JSON.parse(json));
+    }
 });
